a626638f084cc7ae3959dea3a789dd9da1ae4f33
Antoine <antoine.leca@laplateforme.io>
2025-02-11 13:45:30 +0100

    ajout de la page inscription, son css et du fichier root.css

---
+++ inscription.php
@@ -0,0 +1,73 @@
+<?php
+    session_start();
+    require_once(__DIR__ . '/config/connexion.php'); 
+    $erreur_msg = "";
+    if(isset($_POST['submitbutton'])) {
+        extract($_POST);
+        if ($mdp !== $mdp_confirmation) {
+            $erreur_msg = "Les mots de passe ne correspondent pas";
+        } 
+        else {
+            $mdp_hashed = password_hash($mdp, PASSWORD_BCRYPT);
+            $req = $bddPDO->prepare("INSERT INTO utilisateurs (mail, mdp) VALUES (:mail, :mdp)");
+            $req -> execute(
+                array(
+                    'mail' => $mail,
+                    'mdp' => $mdp_hashed
+                )
+            );
+            if ($req->rowCount() > 0) {
+                $_SESSION['utilisateur'] = [
+                    'mail' => $mail,
+                ];
+                header('Location: inscription.php');
+                exit();
+            }
+            else {
+                echo "<p>Erreur lors de l'inscription</p>";
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/root.css">
+    <link rel="stylesheet" href="styles/inscription.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <title>QuizNight - Inscription</title>
+</head>
+<body>
+    <?php require_once(__DIR__ . '/structure/header.php'); ?>
+    <main>
+        <section class="sectionImg">
+            <img src="assets/quiznight.png" alt="logo quiznight">
+        </section>
+        <div class="Bigsection">
+            <h1>Inscription</h1>
+            <section class="formsection">
+                <form action="" method="post">
+                    <label for="">Email</label>
+                    <input placeholder="quiz@night.fr" type="email" name="mail" id="mail" required>
+                    <label for="">Mot de passe</label>
+                    <input type="password" name="mdp" id="mdp" required>
+                    <label for="">Confirmation du mot de passe</label>
+                    <input type="password" name="mdp_confirmation" id="mdp_confirmation" required>
+                    <?php if ($erreur_msg): ?>
+                        <p style="color: red;"><?php echo $erreur_msg; ?></p>
+                    <?php endif; ?>
+                    <div id = "buttonbox">
+                        <button type="submit" name="submitbutton">S'inscrire</button>
+                    </div>
+                </form>
+            </section>
+        </div>
+    </main>
+    <?php require_once(__DIR__ . '/structure/footer.php'); ?>
+</body>
+</html>
