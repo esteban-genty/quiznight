f78a0a3741b008f78a8b0484dd8b74fd77ce29de
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2025-02-11 11:39:18 +0100

    connexion

---
+++ connexion.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="widht=device,initiale-scale=1.0">
+    <title>QuizNight</title>
+   <!------------Google Fonts---------------->
+
+    
+    
+   <!------------Styles Css---------------->
+    <link rel="stylesheet" href="styles/connexion.css">
+</head>
+<body>
+
+<main>
+
+        <img src="" alt="logo">
+        
+       
+        <h1>Connexion</h1>
+        <section class="formsection">
+            
+            <form action="" method="post">
+           
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required>
+
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password" required>
+
+                <div id="buttonbox">
+                    <button type="submit">Se connecter</button>
+                </div>
+            </form>
+        </section>
+
+       
+    </main>
+</body>
+</html>
