3fb0e8929a3f2a4546e07f45514c73a2f32dde58
esteban-genty <esteban.genty@laplateforme.io>
2025-02-12 12:28:02 +0100

    Ajout du dashboard de l'utilisateur

---
+++ utilisateurs/dashboard.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Site de quiz">
+    <meta name="keywords" content="QuizNight, Quiz en ligne">
+    <meta name="author" content="Estéban, Antoine, Sébastien">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QuizNight - Choix de catégories</title>
+
+    <!-- Fichier styles -->
+    <link rel="stylesheet" href="../styles/quiz.css">
+    <link rel="stylesheet" href="../styles/styles.css">
+
+    <!-- Police d'écriture -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+
+        <section class="dashboard">
+
+            <h3>Bienvenue dans votre dashboard, <?php echo "Estéban Genty"; ?></h3>
+            <button class="deconnexion">Déconnexion</button>
+
+
+        </section>
+
+
+
+    </main>
+
+
+
+
+
+</body>
