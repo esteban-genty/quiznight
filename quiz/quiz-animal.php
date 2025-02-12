a11dc6c5f628ec386d437b82de85f9dff27ecefb
esteban-genty <esteban.genty@laplateforme.io>
2025-02-12 12:17:08 +0100

    Ajout de l'affichage des réponses aux question

--- quiz/quiz-animal.php
+++ quiz/quiz-animal.php
@@ -1,10 +1,8 @@
 <?php require_once(__DIR__ . '/../config/connexion.php') ?>
 
-
-
 <?php
 
-class Quiz_animal{
+class Quiz_animal {
 
     private $bddPDO;
 
@@ -12,27 +10,48 @@ class Quiz_animal{
         $this->bddPDO = $bddPDO;
     }
 
-    public function requeteAnimal(){
+    public function requeteAnimal() {
         $requete = "SELECT * FROM animal";
         $requete_animal = $this->bddPDO->prepare($requete);
         $requete_animal->execute();
         return $requete_animal;
     }
 
-    public function afficherQuestion(){
+    public function afficherReponse($id_question, $reponse_utilisateur) {
 
+        $requete_reponse = $this->bddPDO->prepare("SELECT reponses FROM animal WHERE id_question = :id_question");
+        $requete_reponse->bindValue(':id_question', $id_question, PDO::PARAM_INT);
+        $requete_reponse->execute();
+        $reponse_correcte = $requete_reponse->fetch(PDO::FETCH_ASSOC)['reponses'];
+
+        if (strtolower($reponse_utilisateur) == strtolower($reponse_correcte)) {
+            return "<p class='bonne-reponse'>Bonne réponse</p>";
+        } else {
+            return "<p class='mauvaise-reponse'>Mauvaise Réponse</p>";
+        }
+    }
+
+    public function afficherQuestion() {
         $requete_animal = $this->requeteAnimal();
 
         while ($choisir_reponses = $requete_animal->fetch(PDO::FETCH_ASSOC)) {
-                echo '<form action="" method="POST">';
-                echo "<h2>" . $choisir_reponses["questions"] . "</h2>";
-                echo '<button class="vrai" type="submit"> Vrai </button>';
-                echo '<button class="faux" type="submit"> Faux </button>';
-                echo '</form>';
+            echo '<form action="" method="POST">';
+            echo "<h2>" . $choisir_reponses["questions"] . "</h2>";
+
+            echo "<input type='hidden' name='id_question' value='" . $choisir_reponses['id_question'] . "'>";
+            echo '<input type="submit" name="reponse" value="vrai" class="vrai">';
+            echo '<input type="submit" name="reponse" value="faux" class="faux">';
+            echo '</form>';
+
+            
+            if (isset($_POST['reponse']) && $_POST['id_question'] == $choisir_reponses['id_question']) {
+                $reponse_utilisateur = $_POST['reponse'];
+                $id_question = $_POST['id_question'];
+                echo $this->afficherReponse($id_question, $reponse_utilisateur);
+            }
         }
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -68,18 +87,9 @@ class Quiz_animal{
 
         $quiz_animal->afficherQuestion();
         
-        
         ?>
-
-
-
-
 
     </section>
 
-
-
-
-
-
 </body>
+</html>
