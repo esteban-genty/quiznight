346c7fb2da85128e15223cfe8f07fdc99023e828
Antoine <antoine.leca@laplateforme.io>
2025-02-12 09:36:24 +0100

    migration du code classique vers une classe Utilisateur

--- inscription.php
+++ inscription.php
@@ -2,30 +2,37 @@
     session_start();
     require_once(__DIR__ . '/config/connexion.php'); 
     $erreur_msg = "";
-    if(isset($_POST['submitbutton'])) {
-        extract($_POST);
-        if ($mdp !== $mdp_confirmation) {
-            $erreur_msg = "Les mots de passe ne correspondent pas";
-        } 
-        else {
+    class Utilisateur {
+        private $bddPDO;
+    
+        public function __construct($bddPDO) {
+            $this->bddPDO = $bddPDO;
+        }
+    
+        public function inscrire($mail, $mdp, $mdp_confirmation) {
+            if ($mdp !== $mdp_confirmation) {
+                return "Les mots de passe ne correspondent pas";
+            }
+            
+            if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+                return "Adresse e-mail invalide";
+            }
+            
             $mdp_hashed = password_hash($mdp, PASSWORD_BCRYPT);
-            $req = $bddPDO->prepare("INSERT INTO utilisateurs (mail, mdp) VALUES (:mail, :mdp)");
-            $req -> execute(
-                array(
-                    'mail' => $mail,
-                    'mdp' => $mdp_hashed
-                )
-            );
+            
+            $req = $this->bddPDO->prepare("INSERT INTO utilisateurs (mail, mdp) VALUES (:mail, :mdp)");
+            $req->execute([
+                'mail' => $mail,
+                'mdp' => $mdp_hashed
+            ]);
+            
             if ($req->rowCount() > 0) {
-                $_SESSION['utilisateur'] = [
-                    'mail' => $mail,
-                ];
+                $_SESSION['utilisateur'] = ['mail' => $mail];
                 header('Location: inscription.php');
                 exit();
             }
-            else {
-                echo "<p>Erreur lors de l'inscription</p>";
-            }
+            
+            return "Erreur lors de l'inscription";
         }
     }
 ?>
