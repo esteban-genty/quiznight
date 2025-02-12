df43fc8f22f510e333e953c709ade4b438154473
Antoine <antoine.leca@laplateforme.io>
2025-02-12 12:06:37 +0100

    ajout des fichiers footer/header et leurs css

--- inscription.php
+++ inscription.php
@@ -43,7 +43,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/root.css">
+    <link rel="stylesheet" href="styles/header.css">
     <link rel="stylesheet" href="styles/inscription.css">
+    <link rel="stylesheet" href="styles/footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
@@ -52,9 +54,9 @@
 <body>
     <?php require_once(__DIR__ . '/structure/header.php'); ?>
     <main>
-        <section class="sectionImg">
+        <!-- <section class="sectionImg">
             <img src="assets/quiznight.png" alt="logo quiznight">
-        </section>
+        </section> -->
         <div class="Bigsection">
             <h1>Inscription</h1>
             <section class="formsection">
---
+++ structure/header.php
@@ -0,0 +1,10 @@
+<header>
+    <a class="aTitre" href="accueil.php"><h1>Accueil</h1></a>
+    <a class="aImg" href="accueil.php"><img src="assets/quiznight.png" alt="logo quiznight"></a>
+    <nav>
+        <ul>
+            <li><a href="#login.php">Connexion</a></li>
+            <li><a href="#inscription.php">Inscription</a></li>
+        </ul>
+    </nav>
+</header>
