8e8f2333637bcafe75205219fce11810b89811f2
Live Yu Peng Sébastien <sebastien.live-yu-peng@laplateforme.io>
2025-02-14 13:02:58 +0100

    connexion.php

--- utilisateur/connexion.php
+++ utilisateur/connexion.php
@@ -1,18 +1,34 @@
 <?php
-
+session_start();
 // connexion.php
-require_once 'classes/Database.php';
-require_once 'classes/User.php';
+require_once 'utilisateur/connexion-utilisateur.php';
+require_once 'utilisateur/utilisateur.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $mail = $_POST['mail'];
-    $mdp = $_POST['mdp'];
 
-    $database = new Database();
-    $db = $database->connect();
 
-    $auth = new User($db);
-    $error = $auth->login($mail, $mdp);
+if (isset($_SESSION['id_utilisateur'])) {
+    header('Location: dashboard.php');
+    exit;
+}
+
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email'], $_POST['motdepasse'])) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $motdepasse = htmlspecialchars($_POST['motdepasse']);
+
+
+
+    $database = new Connexion('localhost','quiznight','root','');
+    $db = $database->connexionBDD();
+
+    $connexion = new User($db);
+    if ($connexion->login($email, $motdepasse)) {
+        header('Location: dashboard.php');
+        exit;
+    } else {
+        $error = "Email ou mot de passe incorrect.";
+    }
 }
 ?>
 
@@ -40,11 +56,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
             <form method="POST">
                
-                <label for="mail">Email :</label>
-                <input type="email" name="mail" required>
+                <label for="email">Email :</label>
+                <input type="email" name="email" required>
 
-                <label for="mdp">Mot de passe :</label>
-                <input type="password" name="mdp" required>
+                <label for="motdepasse">Mot de passe :</label>
+                <input type="password" name="motdepasse" required>
 
                 <div id="buttonbox">
                     <button type="submit">Se connecter</button>
